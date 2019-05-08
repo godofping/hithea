9350ff5b4363d387a4db2c7ef8eeab56e9c4cfa0
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-05-09 00:08:20 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,33 +1,60 @@
-/*
+/*
+
 SQLyog Ultimate v8.53 
-MySQL - 5.5.5-10.1.38-MariaDB : Database - hithea_db
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`hithea_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `hithea_db`;
-
-/*Table structure for table `answer_table` */
-
-DROP TABLE IF EXISTS `answer_table`;
-
+MySQL - 5.5.5-10.1.38-MariaDB : Database - hithea_db
+
+*********************************************************************
+
+*/
+
+
+
+/*!40101 SET NAMES utf8 */;
+
+
+
+/*!40101 SET SQL_MODE=''*/;
+
+
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hithea_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+
+
+USE `hithea_db`;
+
+
+
+/*Table structure for table `answer_table` */
+
+
+
+DROP TABLE IF EXISTS `answer_table`;
+
+
+
 CREATE TABLE `answer_table` (
   `answerID` int(6) NOT NULL AUTO_INCREMENT,
   `answer` varchar(60) DEFAULT NULL,
   `whenClicked` datetime DEFAULT NULL,
   PRIMARY KEY (`answerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
-/*Data for the table `answer_table` */
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+
+
+/*Data for the table `answer_table` */
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
